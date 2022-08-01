ce6702cbaba829df605b0fea434df95ffacaea50
LoganM3 <mccalleylogan@gmail.com>
2022-08-01 17:03:51 -0400

    alittle css

--- students.js
+++ students.js
@@ -6,8 +6,16 @@ const port = 4001
 app.use(cors())
 app.use(express.json())
 
+let students = ['bob','sam','lilly', 'grace', 'jim']
+
 app.get('/', (req,res)=>{
-    res.send('hello world')
+    let output = "<html><body><ul>" //start
+    //  output = output + "hello there html" //middle
+    for(let i = 0; i < students.length; i++){
+    output = output + '<li><input type = "checkbox"> '+students[i]+'</li>'
+    }
+    output = output + "</ul></body></html>" // end
+    res.send(output)
 })
 
 app.listen(port)
